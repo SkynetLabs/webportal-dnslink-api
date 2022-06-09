2b0fedc5608febdadb370a3bd35abd05cb9855e3
Michał Leszczyk <michasko37@gmail.com>
2022-06-09 14:29:27 +0200

    Configure logger properly

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,12 @@
 const winston = require("winston");
 
+const ERROR_LOG_FILE = "logs/error.log";
+
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   defaultMeta: { service: "dnslink-api" },
-  transports: [
-    new winston.transports.File({ filename: "error.log", level: "error" }),
-    new winston.transports.File({ filename: "log.log" }),
-  ],
+  transports: [new winston.transports.File({ filename: ERROR_LOG_FILE, level: "error" })],
 });
 
 if (process.env.NODE_ENV !== "test") {
